42ff9cbd4951ed2fe682f46ba8d89f33b8792207
ushashir <laleoict@gmail.com>
2023-12-03 17:54:44 +0000

    attendance inner component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,7 @@ import { AnalyticsComponent } from './main-components/analytics/analytics.compon
 import { UsersComponent } from './main-components/users/users.component';
 import { UserDetailsComponetComponent } from './main-components/user-details-componet/user-details-componet.component';
 import { LoginComponent } from './auth/login/login.component';
+import { AttendanceInnerComponent } from './main-components/attendance-inner/attendance-inner.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
@@ -18,6 +19,7 @@ export const routes: Routes = [
         { path: 'attendance', component: AttendanceComponent },
         {path:'',component:DashboardComponent},
         {path:'attendance',component:AttendanceComponent},
+        {path:'attendance-inner/:id',component:AttendanceInnerComponent},
         {path:'leave',component:LeaveComponent},
         {path:'analytics',component:AnalyticsComponent},
         {path:'users',component:UsersComponent},
